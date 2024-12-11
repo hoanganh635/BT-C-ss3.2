449f6d540cfd0aebce83ff858911095ddb0d1ad7
hoanganh635 <hoanganhnv1997@gmail.com>
2024-12-11 21:55:19 +0700

    Add files via upload

---
+++ ac.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[50];
+    printf("moi ban nhap ten cua minh: ");
+    scanf("%49s",name);
+    printf("xin chao %s",name);
+    return 0;
+
+}
